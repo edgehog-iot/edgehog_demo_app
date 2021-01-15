9e69ca51d5f32e54ee90ede7f97a143ba32429a7
Francesco Vaiani <francesco.vaiani@udoo.org>
2021-01-15 16:18:31 +0100

    added file read to logs

--- demo.py
+++ demo.py
@@ -5,5 +5,8 @@ def file_check(path):
     print("Checking {}...".format(path), end='')
     if os.path.exists(path) and os.path.isfile(path):
         print("SUCCESS")
+        with open(path) as fp:
+            for line in fp:
+                print(line.strip())
     else:
         print("FAIL")
